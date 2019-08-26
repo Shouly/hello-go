2ddff9d2ec666c8d3af6518187111cc592099ef1
Shouly <shoulyliang@gmail.com>
2019-08-26 17:12:12 +0800

    add func

--- pointer.go
+++ pointer.go
@@ -2,7 +2,39 @@ package main
 
 import "fmt"
 
+type X struct {
+	name string
+}
+
 func main() {
+
+	////
+
+	x1 := X{name: "1"}
+
+	x1.name = "x1"
+
+	fmt.Println(x1)
+
+	var x2 X
+
+	x2.name = "x2"
+
+	fmt.Println(x2)
+
+	x3 := &X{}
+
+	x3.name = "x3"
+
+	fmt.Println(*x3)
+
+	x4 := new(X)
+
+	x4.name = "x4"
+
+	fmt.Println(*x4)
+
+	/////
 
 	p := new(int)
 
